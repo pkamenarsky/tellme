c531bb65169037a7e87a129126f34941139c6bab
Philip Kamenarsky <pk@harmonious.at>
2012-03-30 12:03:42 +0200

    tests

--- test/tellme/test/session.clj
+++ test/tellme/test/session.clj
@@ -1,30 +1,37 @@
 (ns tellme.test.session
   (:require [tellme.session :as session]
             [aleph.http :as http]
+            [aleph.formats :as formats]
             [lamina.core :as lamina])
   (:use [clojure.test]))
 
 
 ; Fixtures -----------------------------------------------------------------
 
-(defn server-fixture [f]
-  (let [stop (http/start-http-server session/handler {:port 8082})]
+(defn with-server [f]
+  (let [stop (http/start-http-server session/handler {:port 8082})] 
     (f) 
     (stop)))
 
-(use-fixtures :each server-fixture)
+(defn reset-state [f]
+  (require '[tellme.session :as session] :reload)
+  (f))
+
+(use-fixtures :once with-server)
+(use-fixtures :each reset-state)
 
 ; Tests --------------------------------------------------------------------
 
 (defn hget
   "Returns a channel containing the response."
   [addr]
-  (:body
-    (http/sync-http-request
-      {:method :get
-       :url (str "http://localhost:8082/" addr)})))
-
+  (lamina/map*
+    formats/bytes->string
+    (:body
+      (http/sync-http-request
+        {:method :get
+         :url (str "http://localhost:8082/" addr)}))))
 
 (deftest test-backchannel
-  (lamina/receive-all (hget "backchannel") (fn [v] (println v) (is false "3636346"))))
+  (println (first (lamina/lazy-channel-seq (hget "backchannel")))))
 
