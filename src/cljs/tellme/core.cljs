c10b6c7c2e006e98f600aed457b9fd1ad2762c99
Philip Kamenarsky <pk@harmonious.at>
2012-04-03 18:44:56 +0200

    input number restriction

--- src/cljs/tellme/core.cljs
+++ src/cljs/tellme/core.cljs
@@ -1,11 +1,36 @@
 (ns tellme.core
-  (:require-macros [tellme.base.fsm :as fsm]))
+  (:require [goog.dom :as dom]
+            [goog.events.KeyHandler :as keyhandler]
+            [goog.events.KeyCodes :as keycodes]
+            [goog.events :as events])
+  (:use [tellme.base.fsm :only [fsm stateresult data state next-state ignore-msg send-message goto]])
+  (:use-macros [tellme.base.fsm-macros :only [defsm]]))
 
-(def sm (fsm/defsm
+(def sm (defsm
           nil
           ([:start :in data]
            (js/alert "asdasda")
-           (fsm/ignore-msg))))
+           (ignore-msg))
+          
+          ([:start {:keys [a b]} _]
+           (js/alert (+ a b))
+           (ignore-msg))))
 
 (defn test []
+  (-> sm
+    (goto :start) 
+    (send-message {:a 5 :b 7})) 
   (+ 1 3))
+
+(defn main []
+  (let [input (dom/getElement "osid")
+        keyhandler (fn [event]
+                     (let [kcode (.-keyCode event)
+                           ccode (.-charCode event)]
+
+                       (if (and (not= kcode keycodes/BACKSPACE)
+                                (or (< ccode keycodes/ZERO)
+                                    (> ccode keycodes/NINE)))
+                         (.preventDefault event))))]
+    (.focus input)
+    (events/listen (events/KeyHandler. input) "key" keyhandler)))
